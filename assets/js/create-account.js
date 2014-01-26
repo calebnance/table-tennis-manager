84da153eeb1e90b8f7cfdc5498601002ae3051e4
Caleb Nance <calebnance@gmail.com>
2014-01-26 14:19:50 -0500

    Form validation started
    Passwords match,
    Valid e-mail supplied

--- assets/js/create-account.js
+++ assets/js/create-account.js
@@ -13,17 +13,44 @@ $(document).ready(function(){
 	
 	$.validateForm = function(check_empty){
 		$fields = $('form .required');
+		var email_val = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/igm;
 		var submit = true;
 		$.each($fields, function(f, field){
+			var value = $(field).val();
+			var name = $(field).attr('name'); 
 			if(check_empty){
-				if($(field).val() == ''){
+				if(value == ''){
+					$(field).closest('.form-group').addClass('has-error').removeClass('has-success');
 					submit = false;
+				} else {
+					$(field).closest('.form-group').removeClass('has-error').addClass('has-success');
 				}
 			}
 			
-			
-			if($(field).val() != ''){
-					
+			if(value != ''){
+				if(name == 'acco_email'){
+					if(!email_val.test(value)){
+						$(field).prev('label').find('span').html('<small> - Not a valid e-mail address!</small>');
+						$(field).closest('.form-group').addClass('has-error');
+						console.log('not valid');
+					} else {
+						console.log('valid');
+					}
+				}
+				if(name == 'acco_password'){
+					var pass2 = $('#acco_password2').val();
+					if(pass2 != ''){
+						if(value != pass2){
+							$('#acco_password').prev('label').find('span').html('<small> - Passwords do not match</small>');
+							$('#acco_password').closest('.form-group').addClass('has-error').removeClass('has-success');
+							$('#acco_password2').closest('.form-group').addClass('has-error').removeClass('has-success');
+						} else {
+							$('#acco_password').prev('label').find('span').html('<small> - Passwords match!</small>');
+							$('#acco_password').closest('.form-group').removeClass('has-error').addClass('has-success');
+							$('#acco_password2').closest('.form-group').removeClass('has-error').addClass('has-success');
+						}
+					}
+				}
 			}
 		});
 		
--- create-account.php
+++ create-account.php
@@ -23,19 +23,19 @@
 				<form role="form" action="create-account.php" method="POST">
 					
 					<div class="form-group">
-						<label for="acco_name">Name</label>
+						<label class="control-label" for="acco_name">Name <span></span></label>
 						<input type="email" class="form-control required" id="acco_name" name="acco_name" placeholder="Enter Name">
 					</div>
 					<div class="form-group">
-						<label for="acco_email">E-mail</label>
+						<label class="control-label" for="acco_email">E-mail <span></span></label>
 						<input type="email" class="form-control required" id="acco_email" name="acco_email" placeholder="example@gmail.com">
 					</div>
 					<div class="form-group">
-						<label for="acco_password">Password</label>
+						<label class="control-label" for="acco_password">Password <span></span></label>
 						<input type="password" class="form-control required" id="acco_password" name="acco_password" placeholder="Password">
 					</div>
 					<div class="form-group">
-						<label for="acco_password2">Re-Type Password</label>
+						<label class="control-label" for="acco_password2">Re-Type Password <span></span></label>
 						<input type="password" class="form-control required" id="acco_password2" name="acco_password2" placeholder="Password">
 					</div>
 					<button id="submit-create-account" type="submit" class="btn btn-navy">Create Account</button>
