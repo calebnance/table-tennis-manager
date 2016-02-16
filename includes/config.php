3397323913e98a698a592270b65afd8e4e741315
Caleb Nance <calebnance@gmail.com>
2016-02-16 00:59:07 -0500

    spaces

--- includes/config.php
+++ includes/config.php
@@ -16,17 +16,17 @@
 
 	// Paths
 	$base_url	= getBaseUrl($dir_deep);
-	$assets		= $base_url . 'assets/';
-	$css			= $assets . 'css/';
-	$js				= $assets . 'js/';
-	$bs				= $assets . 'bootstrap/';
-	$bs_css		= $bs . 'css/';
-	$bs_js		= $bs . 'js/';
-	$fa				= $assets . 'fontawesome4/';
-	$fa_css		= $fa . 'css/';
-	$dp 			= $assets . 'datepicker/';
-	$dp_js 		= $dp . 'js/';
-	$dp_css 	= $dp . 'css/';
+	$assets = $base_url . 'assets/';
+	$css = $assets . 'css/';
+	$js = $assets . 'js/';
+	$bs = $assets . 'bootstrap/';
+	$bs_css = $bs . 'css/';
+	$bs_js = $bs . 'js/';
+	$fa = $assets . 'fontawesome4/';
+	$fa_css = $fa . 'css/';
+	$dp = $assets . 'datepicker/';
+	$dp_js = $dp . 'js/';
+	$dp_css = $dp . 'css/';
 
 	// Set helper variables
 	$current_page	= getCurrentPage();
@@ -34,20 +34,19 @@
 	// Does this class exist? Errors out when wizard is present, so fixed it.
 	if(class_exists('tt')) {
 		// Database
-		$db_host	= tt::DBHOST;
-		$db_user	= tt::DBUSER;
-		$db_pass	= tt::DBPASS;
-		$db_name	= tt::DBTABLE;
+		$db_host = tt::DBHOST;
+		$db_user = tt::DBUSER;
+		$db_pass = tt::DBPASS;
+		$db_name = tt::DBTABLE;
 	}
 
-	// Season Setup
-	// for each season
+	// Season Setup for each season
 	$weeks = 2;
 	if(date('N') == 1) {
-		// grab today
+		// Grab today
 		$week_start	= date('Y-m-d');
 	} else {
-		// grab last weeks monday
+		// Grab last weeks monday
 		$week_start	= date('Y-m-d', strtotime('next monday'));
 	}
 
@@ -73,7 +72,7 @@
 	 * If a single page needs its own style..
 	 * set $page_styles before header is included
 	 *
-	 * $page_styles		= [];
+	 * $page_styles = [];
 	 * $page_styles[]	= 'page-specific-style.css';
 	 */
 	if(isset($page_styles)){
