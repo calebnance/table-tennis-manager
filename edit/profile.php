733db03d12d60894bb2be73e6bbd837b0370d25f
Caleb Nance <calebnance@gmail.com>
2016-02-16 02:00:04 -0500

    starting of edit profile because YOLO

--- edit/profile.php
+++ edit/profile.php
@@ -1,1 +1,41 @@
-coming soon...
+<?php
+	// Set page title
+	$title = 'Edit Profile';
+
+	// wat
+	$root_path = '../';
+	$dir_deep = 'edit';
+
+	include('../template/header.php');
+?>
+	<div class="container">
+		<div class="row row-offcanvas row-offcanvas-right">
+
+			<div class="col-xs-12 col-sm-9">
+				<p class="pull-right visible-xs">
+					<button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle</button>
+				</p>
+
+				<h1 class="page-header"><?= $title; ?></h1>
+
+				<?php
+				/*
+				Do a textarea limit, show the limit.
+				*/
+				?>
+
+				<div class="clearfix"></div>
+
+				<p class="lead">Coming Soon<span class="loading-dots">.</span></p>
+
+			</div><!-- /.col-xs-12 -->
+
+			<?php
+			include('../template/sidebar.php');
+			?>
+
+		</div><!-- /.row -->
+	</div><!-- /.container -->
+
+<?php
+	include('../template/footer.php');
