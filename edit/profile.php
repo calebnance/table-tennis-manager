819590d629f91156b7c42c0ff820764a1182dc0f
Caleb Nance <calebnance@gmail.com>
2016-02-22 21:07:42 -0500

    start of text limiter js plugin stuff

--- edit/profile.php
+++ edit/profile.php
@@ -7,6 +7,14 @@
 	$dir_deep = 'edit';
 
 	include('../template/header.php');
+
+	// Include page js
+	// are we in dev or prod?
+	if(DEBUG) {
+		$scripts[] = $js . 'dev/edit.js?v=' . $version;
+	} else {
+		$scripts[] = $js . 'edit.min.js?v=' . $version;
+	}
 ?>
 	<div class="container">
 		<div class="row row-offcanvas row-offcanvas-right">
@@ -17,16 +25,26 @@
 				</p>
 
 				<h1 class="page-header"><?= $title; ?></h1>
+				<div class="clearfix"></div>
+
+				<p class="lead">Coming Soon<span class="loading-dots">.</span></p>
 
 				<?php
 				/*
 				Do a textarea limit, show the limit.
 				*/
 				?>
+				<div class="form-element-wrapper margin-b-20">
+					<textarea class="form-control no-resize limit-text" rows="4" data-limit-char="180"></textarea>
+					<div class="form-type-count">0</div><!-- /.form-type-count -->
+				</div><!-- /.form-element-wrapper -->
 
-				<div class="clearfix"></div>
-
-				<p class="lead">Coming Soon<span class="loading-dots">.</span></p>
+				<div class="well">
+					<div class="btn-group">
+						<button class="btn btn-default">Cancel</button>
+						<button class="btn btn-success">Save</button>
+					</div><!-- /.btn-group -->
+				</div><!-- /.well -->
 
 			</div><!-- /.col-xs-12 -->
 
